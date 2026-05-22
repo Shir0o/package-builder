26bd420e1a95c609d78f23efc0a3d2f933c960b3
Tony Wang <yilongwang05@gmail.com>
2026-05-21 20:10:53 -0700

    feat: move document settings to topbar button and restore properties panel hiding

--- src/icons.tsx
+++ src/icons.tsx
@@ -73,4 +73,6 @@ export const Icons = {
   Maximize: ic(
     <path d="M4 9V5a1 1 0 0 1 1-1h4M20 9V5a1 1 0 0 0-1-1h-4M4 15v4a1 1 0 0 0 1 1h4M20 15v4a1 1 0 0 1-1 1h-4" />,
   ),
+  Sliders: ic(<path d="M4 21v-7M4 10V3M12 21v-9M12 8V3M20 21v-5M20 12V3M1 14h6M9 8h6M17 16h6" />),
 };
+
